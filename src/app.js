f4d6d3c6e883ca7bb1575cdc08f2579688c90135
SsuubiGrace <140877776+SsuubiGrace@users.noreply.github.com>
2023-10-09 15:28:42 +0530

    Updated 'ul' elements letter case for consistency.

--- src/app.js
+++ src/app.js
@@ -17,13 +17,10 @@ function formatDate(timestamp){
 
 //Defining a function that I want axios to call when I get the response back from the api
 function displayTemperature(response){
-    console.log(response.data);//Axios puts the actual object inside the response.data(whole object)
-    //console.log(response.data.main.temp);
     let temperatureElement=document.querySelector("#temperature");
     let CityElement=document.querySelector("#city");
     let DescriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
-    //feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
     let feelslikeElement=document.querySelector("#feelslike");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
@@ -33,13 +30,14 @@ function displayTemperature(response){
     CityElement.innerHTML=response.data.name;
     DescriptionElement.innerHTML=response.data.weather[0].description;
     humidityElement.innerHTML=response.data.main.humidity;
-    
+    feelslikeElement.innerHTML=Math.round(response.data.main.feels_like);
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt*1000);
 
 }
 let apiKey="ef536c135f931c48635e435d71067995";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+let city="London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 //fetching the results of the api in javascript using axios
 axios.get(apiUrl).then(displayTemperature);
