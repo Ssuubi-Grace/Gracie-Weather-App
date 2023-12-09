4c7f814f04d783794e066437a3746fb13bd06054
Ssuubi-Grace <muwongegrace2@gmail.com>
2023-12-09 22:31:03 +0530

    added realtime weather

--- src/app.js
+++ src/app.js
@@ -34,22 +34,46 @@ function displayTemperature(response){
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt*1000);
 
+
+    //dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
-let apiKey="ef536c135f931c48635e435d71067995";
+// Loading default city (Mumbai) on page load
+searchCity("London");
+function searchCity(city) {
+  let apiKey="ef536c135f931c48635e435d71067995";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".search-form-input");
+  let city = searchInput.value;
+  if (city) {
+    searchCity(city);
+  } else {
+    alert("Please type a city");
+  }
+}
+
+/*let apiKey="ef536c135f931c48635e435d71067995";
 let city="London";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 //fetching the results of the api in javascript using axios
 axios.get(apiUrl).then(displayTemperature);
-
+*/
 
 //changing the h1 to a searched city
+/*
 function handleSearchSubmit(event) { 
   event.preventDefault();
 let searchInput =document.querySelector(".search-form-input"); 
 let cityElement2=document.querySelector("#city");
 cityElement2.innerHTML =searchInput.value;
-}
+}*/
+
 let searchFormElement =document.querySelector(".search-form"); 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
